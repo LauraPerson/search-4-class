0881f5208f78bbdfb23c5649aa2a4a825b5d5fa2
Laura Person <lauraperson29800@gmail.com>
2020-05-02 14:41:50 -0300

    Add Gem Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,7 @@ Rails.application.configure do
   # class timestamps to determine how long to wait before reading from the
   # replica.
   #
+  config.action_mailer.default_url_options = { host: 'XXXX', port: 80 }
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
   # strategy for connection switching and pass that into the middleware through
