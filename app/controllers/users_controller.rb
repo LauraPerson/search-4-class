d717b4fd82f9a16aa89afaac18785eb46dd9b432
Amanda Juliao <amanda.juliao@hotmail.com>
2020-05-05 21:30:36 -0300

    added names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        current_user.update(params_user)
+        redirect_to user_path(current_user)
+    end
+
+    private
+
+    def params_user
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
+
+
