2fb93215e78ae97c7da5794950168f065bc9f92d
Laura Person <lauraperson29800@gmail.com>
2020-05-06 22:01:32 -0300

    added errors in form

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,9 +4,9 @@ class Course < ApplicationRecord
   belongs_to :category
   validates :title, presence: true
   validates :schedule_date, presence: true
-  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :description, presence: true
-  validates :price, presence: :true, numericality: { greater_than: 0 }
+  validates :price, presence: :true, numericality: { only_integer: true, greater_than: 0 }
   validate :schedule_date_cant_be_past
 
   def schedule_date_cant_be_past
