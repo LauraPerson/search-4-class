5f490dbc0297553e46eae273d847ffdfb228eaa6
Alexis Lenoir <alexis59495@gmail.com>
2020-05-11 17:41:25 -0300

    User show page : delete and edit buttons design + bookings counter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def icon_destroy(name, size = 1)
+    html = "<i class='far fa-#{name}-alt' "
+    html += "style='font-size:#{size}em' "
+    html += "></i>"
+    html.html_safe
+  end
+
+  def icon_edit(name, size = 1)
+    html = "<i class='far fa-#{name}' "
+    html += "style='font-size:#{size}em' "
+    html += "></i>"
+    html.html_safe
+  end
 end
