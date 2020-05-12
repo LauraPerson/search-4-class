19f3d961d9feff5d0c609cf39feac8188c79c572
Andressa DAgostini <ad.dagostini@gmail.com>
2020-05-12 20:41:51 -0300

    Added address, lat e long to Courses Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_014955) do
+ActiveRecord::Schema.define(version: 2020_05_12_234028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 2020_05_06_014955) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.text "address"
     t.index ["category_id"], name: "index_courses_on_category_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
